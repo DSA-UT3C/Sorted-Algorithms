918d7bb0681173d93603fc6854deadb83624b482
Tony1411 <78512236+Tony1411@users.noreply.github.com>
2022-01-12 21:23:43 -0500

    Update HeapSort.cpp

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -8,19 +8,19 @@
  * @param idx índice de la raíz
  */
 
-void heapify(int arr[], int V, int idx)
+void heapify(int* arr, int V, int idx)
 {
     int root = idx;
     int left = 2 * idx + 1;
     int right = 2 * idx + 2;
 
     // Máximo entre los dos hijos del root
-    if (left <= V and arr[left] > arr[root])
+    if (left < V and arr[left] > arr[root])
     {
 	    root = left;
     }
 
-    if (right <= V and arr[right] > arr[root])
+    if (right < V and arr[right] > arr[root])
     {
 	    root = right;
     }
@@ -38,14 +38,27 @@ void heapify(int arr[], int V, int idx)
 }
 
 
-void HeapSort(int arr[], int V){
-
+void HeapSort(int* arr, int V){
+    for (int i = V/2 - 1; i >= 0; --i){
+        heapify(arr, V, i);      
+    }          
+  
+    for (int i = V - 1; i >= 0; --i) {
+        std::swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }  
 };
 
 int main()
 {
     int arr[] = { 12, 11, 13, 5, 6, 7 };
     int n = sizeof(arr) / sizeof(arr[0]);
-    heapify(arr, n, 2);
+    HeapSort(arr, n);
+  
+    for(int i = 0; i < n; ++i){
+       std::cout<<arr[i]<<" ";
+    } 
+    std::cout<<std::endl;
+	
     return 0;
 }
